b2ee512a212ac4fb921fd02a39e26006cc610ec1
0R1-qt <orly.plaza@hcdc.edu.ph>
2022-02-15 15:27:39 +0800

    Student Form Added

--- DysIMALPlayGround/main.py
+++ DysIMALPlayGround/main.py
@@ -3,12 +3,19 @@ from PyQt5 import uic
 import sys
 
 
-class Main(QMainWindow):
+class Main(QDialog):
     def __init__(self):
         QMainWindow.__init__(self)
-        uic.loadUi("main.ui", self)
-        self.actionsave_and_exit.triggered.connect(sys.exit)
-        self.actionexit.triggered.connect(sys.exit)
+        # uic.loadUi("main.ui", self)
+        # self.actionsave_and_exit.triggered.connect(sys.exit)
+        # self.actionexit.triggered.connect(sys.exit)
+        self.studentDialogForm()    
+
+    def studentDialogForm(self):
+        uic.loadUi("studentForm.ui", self)
+
+    def secondWindowForm(self):
+        uic.loadUi("secondWindow.ui", self)
 
 
 app = QApplication(sys.argv)
