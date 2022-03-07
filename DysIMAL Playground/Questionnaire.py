acbdddef0b70138192dfb79918377a666f16189c
0R1-qt <orly.plaza@hcdc.edu.ph>
2022-03-07 11:52:11 +0800

    Questionnaire Update

--- DysIMAL Playground/Questionnaire.py
+++ DysIMAL Playground/Questionnaire.py
@@ -3,33 +3,38 @@ Question = [
      "num2": "42 + 9 = ",
      "num3": "98 – 81 = ",
      "num4": "66 – 16 = ",
-     "num5": "11 x 7 = ",
-     "num6": "16 x 5 = ",
+     "num5": "11 x 7 = "},
+     {"num6": "16 x 5 = ",
      "num7": "6 ÷ 11 = ",
      "num8": "7 ÷ 15 = ",
      "num9": "Add. 1/4 + 1/2 = ",
-     "num10": "Add. 3/10 + 6/10 = ",
+     "num10": "Add. 3/10 + 6/10 = "},
+     {
      "num11": "Subtract. 40/77 - 15/77 = ",
      "num12": "Subtract. 19/22 - 10/22 = ",
      "num13": "Multiply. 2/3 x 2/5 = ",
      "num14": "Multiply. 1/6 x 3/5 = ",
-     "num15": "Divide. 3/4 ÷ 6/7 = ",
+     "num15": "Divide. 3/4 ÷ 6/7 = "},
+     {
      "num16": "(19 + 7) – 6 + 5 = ",
      "num17": "84 + 6 - (14 – 8) + 2 = ",
      "num18": "19 x 2 ÷ (6 ÷ 2) = ",
      "num19": "(36 ÷ 4) x 2 ÷ 2 = ",
-     "num20": "5.Write the 5-digit number._________ = 30,000 + 100 + 4",
+     "num20": "5.Write the 5-digit number._________ = 30,000 + 100 + 4"},
+    {
      "num21": "6.Write the 6-digit number.__________ = 100,000 + 30,000 + 3,000 + 600 + 4",
      "num22": "7.Write the 5-digit number with decimals.__________ = 50 + 5 + 0.2 + 0.06 + 0.006",
      "num23": "8.Write the 6-digit number with decimals.__________ = 6,000 + 400 + 70 + 9 + 0.6 + 0.08",
      "num24": "9.Round to the nearest ten. 827 = __________",
-     "num25": "10.Round to the nearest hundred. 7,944 = __________",
+     "num25": "10.Round to the nearest hundred. 7,944 = __________"},
+    {
      "num26": "11.Round to the nearest thousand. 18,591 = __________",
      "num27": "Write 8/10 in simplest form. __________ ",
      "num28": "Write 4/16 in simplest form. __________ ",
      "num29": "What is 78% of 100? __________ ",
-     "num30": "25% of what number is 12?  __________ ",
+     "num30": "25% of what number is 12?  __________ "},
+    {
      "num31": "1.Find the missing place value from a 5-digit number. 90,000 + 9,000 + 600 + __________ + 8 = 99,638 ",
-     "num32": "2.Find the missing place value from a 6-digit number. 900,000 + __________ + 1,000 + 300 + 20 + 3 = 991,323", 
-     "num33":
+     "num32": "2.Find the missing place value from a 6-digit number. 900,000 + __________ + 1,000 + 300 + 20 + 3 = 991,323",
+    }
      
